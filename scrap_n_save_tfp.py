ca4ec69ddd5ef417449070bfabd7d198722a4d97
Cahya Alkahfi <cahyaalkahfi@gmail.com>
2022-09-14 06:10:28 +0000

    update code

---
+++ scrap_n_save_tfp.py
@@ -0,0 +1,75 @@
+# Library to use
+
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+import urllib.request as r
+import os
+import urllib.parse as up
+import psycopg2
+import psycopg2.extras as extras
+from datetime import date
+
+# Homepage Wikipedia (English)
+wikipedia_url = "https://en.wikipedia.org/wiki/Main_Page"
+
+
+# Function to get data from "Today's Featured Picture" Section
+def wiki_tfd(url):
+    page = r.urlopen(url)
+    soup = bs(page.read())
+    part_ftd = soup.findChild('div', {'id': 'mp-tfp'})
+    img = "https:" + part_ftd.find('img')['src']
+
+    item = ''
+    contents = part_ftd.find_all('p')
+
+    for idx, i in enumerate(contents):
+        content = i.text.replace('\n', '')
+
+        if len(item) > 280:
+            splits = item.split('.')
+            item = splits[0] + ". "
+
+        item = item + " " + content
+
+    today = date.today().strftime("%B %d, %Y")
+
+    return [{'date': today, 'item': str(item), 'picture': img}]
+
+
+tfd = wiki_tfd(wikipedia_url)
+data = pd.DataFrame(tfd)
+
+# Connect to PostgreSQL DB
+up.uses_netloc.append("postgres")
+url = up.urlparse(os.getenv("DATABASE_URL"))
+print(url)
+conn = psycopg2.connect(database=url.path[1:],
+                        user=url.username,
+                        password=url.password,
+                        host=url.hostname,
+                        port=url.port
+                        )
+
+
+# Function to insert data into PGSQL DB
+def insert_values(conn, df, table):
+    tuples = [tuple(x) for x in df.to_numpy()]
+    cols = ','.join(list(df.columns))
+
+    query = "INSERT INTO %s(%s) VALUES %%s" % (table, cols)
+    cursor = conn.cursor()
+    try:
+        extras.execute_values(cursor, query, tuples)
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as ex:
+        print("Error: %s" % ex)
+        conn.rollback()
+        cursor.close()
+        return 1
+    print("Success")
+    cursor.close()
+    conn.close()
+
+
+insert_values(conn, data, "wikipedia_tfp")
---
+++ tweet_tfp.py
@@ -0,0 +1,74 @@
+import tweepy
+import urllib.parse as up
+import psycopg2
+import pandas as pd
+import os
+import requests
+from datetime import date
+
+
+def main():
+
+    today = date.today().strftime("%B %d, %Y")
+    # Connect to PostgreSQL DB
+    up.uses_netloc.append("postgres")
+    url = up.urlparse(os.environ["DATABASE_URL"])
+    conn = psycopg2.connect(database=url.path[1:],
+                            user=url.username,
+                            password=url.password,
+                            host=url.hostname,
+                            port=url.port
+                            )
+
+    cur = conn.cursor()
+
+    cur.execute(
+        """
+        SELECT *
+        FROM wikipedia_tfp w
+        WHERE w.date = %s;
+        """,
+        [today, ]
+    )
+
+    data = pd.DataFrame(cur.fetchall(),
+                        columns=['id', 'date', 'item', 'picture'])
+
+    cur.close()
+    conn.close()
+
+    # POST TO TWITTER
+    auth = tweepy.OAuthHandler(
+        os.environ['TWITTER_CONSUMER_API_KEY'],
+        os.environ['TWITTER_CONSUMER_API_SECRET']
+    )
+
+    auth.set_access_token(
+        os.environ['ACCESS_TOKEN'],
+        os.environ['ACCESS_TOKEN_SECRET']
+    )
+
+    api = tweepy.API(auth)
+    
+    tweet = data.iloc[0]['item'] + "\n" + \
+        "#wikipedia #todaysfeaturedpicture"
+    print(len(tweet))
+    tweet_with_img(api, data.iloc[0]['picture'], tweet)
+
+
+def tweet_with_img(api, url, message):
+    filename = 'temp.jpg'
+    request = requests.get(url, stream=True)
+    if request.status_code == 200:
+        with open(filename, 'wb') as image:
+            for chunk in request:
+                image.write(chunk)
+
+        api.update_status_with_media(status=message, filename=filename)
+        os.remove(filename)
+    else:
+        print("Image not found")
+
+
+if __name__ == "__main__":
+    main()
